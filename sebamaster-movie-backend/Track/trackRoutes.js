81c776ca2f773885e725bdf1c0e73fbe1f51024e
POM15_yekaterina.salazgorskaya <yekaterina.salazgorskaya@tum.de>
2016-06-27 16:28:51 +0200

    tracks are added

--- sebamaster-movie-backend/Track/trackRoutes.js
+++ sebamaster-movie-backend/Track/trackRoutes.js
@@ -5,22 +5,13 @@ function trackRoutes(passport) {
 
     var trackController = require('./trackController');
     var router = require('express').Router();
-    var unless = require('express-unless');
-    var multer = require('multer');
-    var fs = require('fs');
-    var mw = passport.authenticate('jwt', {session: false});
-    mw.unless = unless;
-
-    //middleware
-    router.use(mw.unless({method: ['GET', 'OPTIONS']}));
-
+ 
     router.route('/tracks')
         .post(trackController.postTrack)
         .get(trackController.getTracks);
 
-    router.route('/tracks/:track_id')
+    router.route('/tracks/:trackId')
         .get(trackController.getTrack)
-        .put(trackController.rateTrack)
         .delete(trackController.deleteTrack);
 
     return router;
--- sebamaster-movie-backend/Track/trackSchema.js
+++ sebamaster-movie-backend/Track/trackSchema.js
@@ -4,7 +4,9 @@ var mongoose = require('mongoose');
 // Define our movie schema
 var Track   = new mongoose.Schema({
     title: String,
-    rating: {type: Number, default: 3 ,min: 0, max: 5,}
+    rating: {type: Number, default: 3 ,min: 0, max: 5,},
+    artist: String,
+    points: Number
 });
 
 // Export the Mongoose model
